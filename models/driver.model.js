a84a6ca180efd58a4e5a99c3f2ac2dd335a89f5a
Dayna Tynan <Dayna_Regan@yahoo.com>
2023-09-05 15:18:28 -0400

    final day model and route changes

--- models/driver.model.js
+++ models/driver.model.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose;
+const DriverSchema = new mongoose.Schema({
+first_name: String,
+last_name: String,
+Vehicles: [{
+    modelYear: Number,
+    make: String,
+    model: String
+}]
+})
 
-const Driver = new Schema(
-  {
-    first_name: { type: String },
-    last_name: { type: String },
-  }
-);
-
-module.exports = mongoose.model('drivers', Driver);
+module.exports = mongoose.model("drivers",DriverSchema)
--- routes/driver.routes.js
+++ routes/driver.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Driver = require('../models/driver.model')
 
-//pull all drivers
+//get all drivers
 router.get('/', (req, res) => {
   Driver.find((err, drivers) => {
       if (err) {
@@ -12,6 +12,7 @@ router.get('/', (req, res) => {
     });
   });
 
+//get specific driver by id
  router.get('/:id',(req, res) => {
     const query = {};
     Driver.findById(req.params.id, (err, drivers) => {
@@ -21,5 +22,78 @@ router.get('/', (req, res) => {
       return res.json(drivers);
     });
   });
+
+//add a new driver
+  router.post('/newDriver', async(req, res) => {
+    console.log(req.body)
+    const FirstName = req.body.first_name
+    const LastName = req.body.last_name
+    const makeName = req.body.make
+    const modelName = req.body.model
+    const modelYear = req.body.modelyear
+
+    const formData = new Driver({
+      first_name: FirstName,
+      last_name: LastName,
+      Vehicles: [{
+        modelYear: modelYear,
+        make: makeName,
+        model:modelName
+    }]
+    })
+    try {
+      formData.save();
+      res.send("inserted data..")
+  } catch(err) {
+      console.log(err)
+  }
+  });
+
+
+//add vehicle to a driver by a specific driver by id
+router.route('/').patch(function(req,res){
+  const driverId = req.body.id
+  const makeName = req.body.make
+  const modelName = req.body.model
+  const modelYear = req.body.modelyear
+  Driver.findByIdAndUpdate(driverId, {$push:{Vehicles:[{model:modelName, modelYear: modelYear, make: makeName}]}},
+  function(err, result){
+
+    if(err){
+        res.send(err)
+    }
+    else{
+        res.send(result)
+        console.log("updated vehicles...")
+  }
+
+})
+});
+
+//Delete specific driver by id
+router.delete('/:id',(req, res) => {
+  const query = {};
+  Driver.findByIdAndDelete(req.params.id, (err, drivers) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(drivers);
+    console.log("Driver Deleted")
+  });
+});
+
+// Delete a vehicle by id
+router.delete('/:userid/vehicles/:vehicleid',(req,res) => {
+  console.log('route called')
+  Driver.findByIdAndUpdate(req.params.userid, {$pull : {Vehicles:{_id: req.params.vehicleid}}} ,(err, drivers)=>{
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(drivers);  
+  })
+})
+
+
+
 
 module.exports = router;
